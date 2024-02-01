5b8e5b1ead8038e59147984288450930354f4ace
michelmb-dev <michel.mb.dev@gmail.com>
2024-02-01 13:19:21 +0100

    feat: customize status line
    - customize lualine theme
    - displaying filename by incline.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -64,4 +64,56 @@ return {
       }
     end,
   },
+
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        -- globalstatus = false,
+        theme = "catppuccin",
+      },
+    },
+  },
+
+  -- customize filename
+  {
+    "b0o/incline.nvim",
+    dependencies = {
+      "catppuccin",
+    },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("catppuccin.palettes").get_palette("mocha")
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = {
+              guibg = colors.lavender,
+              guifg = colors.base,
+            },
+            InclineNormalNC = {
+              guifg = colors.lavender,
+              -- guibg = colors.surface2,
+            },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 0 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
 }
