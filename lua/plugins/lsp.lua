f03d3d0635c807d3cafed5b3b10c1024aa28bb45
michelmb-dev <michel.mb.dev@gmail.com>
2024-03-05 16:28:12 +0100

    fix: lsp for php

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,15 +16,17 @@ return {
         "css-lsp",
         "dockerfile-language-server",
         "docker-compose-language-service",
-        "emmet-language-server",
+        "emmet-ls",
         "eslint-lsp",
         "html-lsp",
         "htmx-lsp",
         "json-lsp",
         "intelephense",
+        "phpactor",
+        "pint",
         "sqlls",
         "yaml-language-server",
-        "twiggy-language-server",
+        -- "twiggy-language-server",
       },
       ui = {
         border = "rounded",
@@ -34,9 +36,149 @@ return {
 
   {
     "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        cssls = {},
+        tailwindcss = {
+          root_dir = function(...)
+            return require("lspconfig.util").root_pattern(".git")(...)
+          end,
+        },
+        emmet_ls = {
+          filetypes = {
+            "html",
+            "templ",
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "php",
+            "twig",
+          },
+        },
+        tsserver = {
+          root_dir = function(...)
+            return require("lspconfig.util").root_pattern(".git")(...)
+          end,
+          single_file_support = false,
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "literal",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
+        },
+        -- twiggy_language_server = {
+        --   root_dir = function(...)
+        --     return require("lspconfig.util").root_pattern("composer.json", ".git")(...)
+        --   end,
+        --   single_file_support = true,
+        -- },
+        html = {
+          filetypes = { "html", "templ", "twig" },
+        },
+        yamlls = {
+          settings = {
+            yaml = {
+              keyOrdering = false,
+            },
+          },
+        },
+        lua_ls = {
+          -- enabled = false,
+          single_file_support = true,
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = {
+                workspaceWord = true,
+                callSnippet = "Both",
+              },
+              misc = {
+                parameters = {
+                  -- "--log-level=trace",
+                },
+              },
+              hint = {
+                enable = true,
+                setType = false,
+                paramType = true,
+                paramName = "Disable",
+                semicolon = "Disable",
+                arrayIndex = "Disable",
+              },
+              doc = {
+                privateName = { "^_" },
+              },
+              type = {
+                castNumberToInteger = true,
+              },
+              diagnostics = {
+                disable = { "incomplete-signature-doc", "trailing-space" },
+                -- enable = false,
+                groupSeverity = {
+                  strong = "Warning",
+                  strict = "Warning",
+                },
+                groupFileStatus = {
+                  ["ambiguity"] = "Opened",
+                  ["await"] = "Opened",
+                  ["codestyle"] = "None",
+                  ["duplicate"] = "Opened",
+                  ["global"] = "Opened",
+                  ["luadoc"] = "Opened",
+                  ["redefined"] = "Opened",
+                  ["strict"] = "Opened",
+                  ["strong"] = "Opened",
+                  ["type-check"] = "Opened",
+                  ["unbalanced"] = "Opened",
+                  ["unused"] = "Opened",
+                },
+                unusedLocalExclude = { "_*" },
+              },
+              format = {
+                enable = false,
+                defaultConfig = {
+                  indent_style = "space",
+                  indent_size = "2",
+                  continuation_indent_size = "2",
+                },
+              },
+            },
+          },
+        },
+      },
+      setup = {},
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
     opts = function()
       -- force border on ui lsp
-      require("lspconfig.ui.windows").default_options.border = "rounded"
+      local windows = require("lspconfig.ui.windows")
+      windows.default_options.border = "rounded"
     end,
   },
 }
